98e9d1a017e70387348cd0cbd8963a3b6bcafe6c
Enyo Fuhrmann <amarenyo@gmail.com>
2025-12-31 12:13:43 +0100

    Fixed thread blocking

--- src/main/java/ch/amarenyo/redisconnector/RedisConnector.java
+++ src/main/java/ch/amarenyo/redisconnector/RedisConnector.java
@@ -1,43 +1,59 @@
 package ch.amarenyo.redisconnector;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
 import redis.clients.jedis.RedisClient;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public final class RedisConnector extends JavaPlugin {
     //Starting connection with redis server
     RedisClient redis = RedisClient.builder().hostAndPort("localhost", 6379).build();
+    private static RedisConnector instance;
 
     @Override
     public void onEnable() {
+
+        instance = this;
+
 
         //test connection, will output PONG on server start if it was successful
         String ping = redis.ping();
         getLogger().info(ping);
 
+
         //Set key color to red on the server
         redis.sadd("color", "Red");
 
         //start an executor to run sub in background
-        ExecutorService executor;
-        executor = Executors.newSingleThreadExecutor();
 
-        //run the executor
-        executor.submit(() -> {
-            try (Jedis jedis = new Jedis()) {
+        Bukkit.getScheduler().runTaskAsynchronously(RedisConnector.getInstance(), () -> {
 
-                jedis.subscribe(new JedisPubSub() {
+        try (Jedis jedis = new Jedis()) {
+
+            jedis.subscribe(new JedisPubSub() {
 
                 @Override
                 public void onMessage(String channel, String message) {
-                    //function that happen on the arrival of a message
+                    if (channel.equals("getSurvival.Server.All") && message.contains("Survival")) {
+                        getLogger().info("Incomming Request getSurvival.Server.All");
+                        Server server = Bukkit.getServer();
+                        int onlineplayers = server.getOnlinePlayers().size();
+                        int maxplayers = server.getMaxPlayers();
 
+                        getLogger().info(onlineplayers + " " + maxplayers);
+
+                        jedis.publish("survival.Server.All", onlineplayers + " / " + maxplayers);
+                        getLogger().info("Published: " + onlineplayers + " / " + maxplayers);
+                    } else if (channel.equals("getSurvival.Player.All")) {
+
+                    } else {
+                        getLogger().warning("If you see this message i fucked up");
+                    }
                 }
-            }, "channel", "channel2"); //specify on which channels should get listened
-            }
+            }, "getSurvival.Server.All", "getSurvival.Player.All"); //specify on which channels should get listened
+        }
         });
     }
 
@@ -46,4 +62,9 @@ public final class RedisConnector extends JavaPlugin {
         redis.close();//close connection on shutdown
         getLogger().info("Shutdown redis connection");
     }
+
+    public static RedisConnector getInstance() {
+        return instance;
+    }
+
 }
